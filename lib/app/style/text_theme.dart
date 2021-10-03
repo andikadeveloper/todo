11405d8391a4a7edc343d1e6b046cd3b2281bc5a
Muhammad Andika <andikarastafara93@gmail.com>
2021-10-03 15:50:52 +0700

    refactor: fix textTheme size

--- lib/app/style/text_theme.dart
+++ lib/app/style/text_theme.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 final textTheme = TextTheme(
   headline6: TextStyle(
     fontFamily: 'Montserrat',
     fontWeight: FontWeight.w800,
     color: Colors.white,
+    fontSize: Get.height * 0.035,
   ),
   bodyText1: TextStyle(
     fontFamily: 'Montserrat',
+    fontSize: Get.height * 0.025,
   ),
 );
